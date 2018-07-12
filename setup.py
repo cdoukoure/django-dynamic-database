585bb011e7589c1282ec4999ac66421360cd7d92
DOUKOURE Jean Charles <doukourejeancharles@MacBook-Pro-de-Roger-KOUAKOU.local>
2018-07-12 15:39:05 +0000

    Editing setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-from django_multitenant.django_multitenant import *
+from setuptools import setup, find_packages
+
+setup(
+    name='django-pivot-models',
+    version='0.1.0',
+    description='Create dynamic physical database with pivot table concept',
+    url='https://github.com/cdoukoure/django-pivot-models',
+    download_url='https://github.com/cdoukoure/django-pivot-models/archive/master.zip',
+    author='Jean-Charles DOUKOURE',
+    author_email='c.doukoure@outlook.fr',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages(),
+    install_requires=['django>=1.10']
+)
