e24bd24ea6cb60a5ddc499917dd20821412c0acf
DOUKOURE Jean Charles <doukourejeancharles@MacBook-Pro-de-Roger-KOUAKOU.local>
2018-08-18 12:02:58 +0000

    Removing MySQL, and SQLite from travis ci test

--- django_dynamic_database/tests/dynamic_database/test.py
+++ django_dynamic_database/tests/dynamic_database/test.py
@@ -77,14 +77,6 @@ class DynamicDBModelModelTests(TestCase):
         self.assertEqual(len(all), cnt)
     
 
-        # Support Aggregation
-        sum_rate = KingBook.objects.aggregate(models.Sum('rate')) # Important! Use custom Sum function to avoid bug with postgres database
-        self.assertEqual(sum_rate, {'rate__sum': 8.5})
-
-        higher_rate = KingBook.objects.aggregate(models.Max('rate'))
-        self.assertEqual(higher_rate, {'rate__max': '5'})
-    
-    
         # Support Save method
         bk8 = KingBook(name="Brad Pete")
         bk8.save()
@@ -96,4 +88,14 @@ class DynamicDBModelModelTests(TestCase):
         bk8.save()
         self.assertEqual(bk8.rate, 2.33)
 
+        # Support Aggregation
+        higher_rate = KingBook.objects.aggregate(models.Max('rate'))
+        self.assertEqual(higher_rate, {'rate__max': '5'})
+        higher_rate = KingBook.objects.aggregate(models.Min('rate'))
+        self.assertEqual(higher_rate, {'rate__max': '1.0'})
+        # SUM bugs with Postgres db
+        sum_rate = KingBook.objects.aggregate(models.Sum('rate'))
+        self.assertEqual(sum_rate, {'rate__sum': 8.5})
+    
+    
 
